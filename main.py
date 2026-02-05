3c874c1fddb07813cbe34a3280ebf728f783aa3a
govindvish9879-pixel <govindvish9879@gmail.com>
2026-02-05 16:38:06 +0545

    Update logger name and bot configuration values

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ from pyrogram import Client, idle
 import asyncio, logging
 
 logging.basicConfig(level=logging.INFO)
-LOGGER = logging.getLogger(__name__)
+LOGGER = logging.getLogger(sinha)
 LOGGER.info("Live log streaming to telegram.")
 plugins = dict(root="plugins")
 if __name__ == "__main__":
     bot = Client(
         "Master",
-        bot_token=Config.BOT_TOKEN,
-        api_id=Config.API_ID,
-        api_hash=Config.API_HASH,
+        bot_token=Config.8160173494:AAEC4vBsfo5z89sGYRLD-M2a6-gEfSlQ8U8,
+        api_id=Config.33383151,
+        api_hash=Config.a1b51f828a72a0b89df9c0db86a601e1,
         sleep_threshold=120,
         plugins=plugins,
         workers=10000,
